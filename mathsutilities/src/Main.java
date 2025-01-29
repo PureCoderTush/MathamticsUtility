01ac140ad9036d84e2cb2091cd0cb09b4e0319e7
ShubhamSthul <sthul.shubham123@gmail.com>
2025-01-29 20:54:02 +0530

    one more new line added

--- mathsutilities/src/Main.java
+++ mathsutilities/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("new methods are added in project");
+        System.out.println("one more new line added to main");
     }
 }
