95bd752e213c61c33425a97fbb5093e28bd17db1
ShubhamSthul <sthul.shubham123@gmail.com>
2025-01-29 20:26:45 +0530

    adding main and addition class reading data from file method written

---
+++ mathsutilities/src/Substraction.java
@@ -0,0 +1,15 @@
+public class Substraction {
+
+    public int sub1(int num1,int num2){
+        return num1-num2;
+    }
+    public float sub2(int num1,int num2){
+        return num1-num2;
+    }
+    public double sub3(int num1,int num2){
+        return num1-num2;
+    }
+    public long sub4(int num1,int num2){
+        return num1-num2;
+    }
+}
